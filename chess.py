0a6b9612fb77735f9fe725d5fa818aa7e3c841ab
Muhammad Faizan <itsmfaizan5@gmail.com>
2026-06-24 14:42:28 +0500

    Display window and set up using GUI.py

---
+++ chess.py
@@ -0,0 +1,23 @@
+import GUI
+import pygame
+from sys import exit
+
+pygame.init()
+
+width=800
+height=600
+
+screen=pygame.display.set_mode((width,height))
+pygame.display.set_caption('Shatranj')
+
+GUI.displayBoard(screen)
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit
+
+    pygame.display.update()
+
+    
